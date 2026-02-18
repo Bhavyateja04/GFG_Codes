f4194dde551617d0baebafe2b9b2065eaf9d52be
Bhavya Teja Penke <147074331+Bhavyateja04@users.noreply.github.com>
2026-02-18 21:52:59 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Count Inversions/count-inversions.cpp
+++ Difficulty: Medium/Count Inversions/count-inversions.cpp
@@ -1,43 +1,49 @@
 class Solution {
   public:
-  
-    long long merge(vector<int>& arr, int left, int mid, int right) {
-        vector<int> temp;
-        int i = left, j = mid + 1;
-        long long inv = 0;
-
-        while (i <= mid && j <= right) {
-            if (arr[i] <= arr[j]) {
-                temp.push_back(arr[i++]);
-            } else {
-                temp.push_back(arr[j++]);
-                inv += (mid - i + 1); 
+    int Merge(vector<int> &arr, int l, int m, int r)
+    {
+        vector<int> merged(r-l+1);
+        int i = l, j = m+1, k = 0, cnt = 0;
+        while(i<m+1 and j<r+1){
+            if(arr[i]>arr[j]){
+                merged[k] = arr[j];
+                cnt+=(m-i+1);
+                j++;
             }
+            else{
+                merged[k] = arr[i];
+                i++;
+            }
+            k++;
         }
-
-        while (i <= mid) temp.push_back(arr[i++]);
-        while (j <= right) temp.push_back(arr[j++]);
-
-        for (int k = left; k <= right; k++)
-            arr[k] = temp[k - left];
-
-        return inv;
+        while(i<m+1){
+            merged[k] = arr[i];
+            i++;
+            k++;
+        }
+        while(j<r+1){
+            merged[k] = arr[j];
+            j++;
+            k++;
+        }
+        
+        for(int i = 0;i<(r-l+1);i++)
+        {
+            arr[l+i] = merged[i];
+        }
+        return cnt;
     }
-
-    long long mergeSort(vector<int>& arr, int left, int right) {
-        if (left >= right) return 0;
-
-        int mid = (left + right) / 2;
-
-        long long inv = 0;
-        inv += mergeSort(arr, left, mid);
-        inv += mergeSort(arr, mid + 1, right);
-        inv += merge(arr, left, mid, right);
-
-        return inv;
+    int MergeSort(vector<int> &arr, int l, int r){
+        if(l>=r)return 0;
+        int m = l + (r-l)/2;
+        int lc = MergeSort(arr, l, m);
+        int rc = MergeSort(arr, m+1, r);
+        
+        int mc = Merge(arr, l, m, r);
+        
+        return lc+rc+mc;
     }
-
-    long long inversionCount(vector<int> &arr) {
-        return mergeSort(arr, 0, arr.size() - 1);
+    int inversionCount(vector<int> &arr) {
+        return MergeSort(arr, 0, arr.size()-1);
     }
 };
