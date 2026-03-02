f21fe69f2dba6ad62f6218b9704dc2a302a61e04
Bhavya Teja Penke <147074331+Bhavyateja04@users.noreply.github.com>
2026-03-02 19:12:09 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Check Prime/check-prime.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // code here
+    int n;
+    cin>>n;
+    bool isPrime = true;
+    if(n<=1) isPrime = false;
+    else{
+        for(int i = 2;i<n;i++){
+            if(n%i == 0){
+                isPrime = false;
+                break;
+            }
+        }
+    }
+    if(isPrime) cout<<"True";
+    else cout<<"False";
+
+    return 0;
+}
