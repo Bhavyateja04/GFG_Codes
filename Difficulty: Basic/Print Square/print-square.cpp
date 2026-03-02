95c236c6320b353a36e613bfa6efe205ad0e7bbd
Bhavya Teja Penke <147074331+Bhavyateja04@users.noreply.github.com>
2026-03-02 19:09:20 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Print Square/print-square.cpp
+++ Difficulty: Basic/Print Square/print-square.cpp
@@ -1,10 +1,7 @@
-#include <iostream>
-using namespace std;
+// User function Template for C++
 
-int main() {
+void square(int n) {
     // code here
-    int n;
-    cin>>n;
     for(int i=0;i<n;i++){
         for(int j=0;j<n;j++){
              if (i == 0 || i == n - 1 || j == 0 || j == n - 1) {
@@ -15,5 +12,4 @@ int main() {
         }
         cout << endl;
     }
-    return 0;
 }
