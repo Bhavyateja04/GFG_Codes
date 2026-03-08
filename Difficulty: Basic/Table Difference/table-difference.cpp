8969629dee6b8ae540f5589decc8472694857b3d
Bhavya Teja Penke <147074331+Bhavyateja04@users.noreply.github.com>
2026-03-08 22:17:38 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Table Difference/table-difference.cpp
@@ -0,0 +1,21 @@
+// User function Template for C++
+
+void difference(int n1, int n2) {
+
+    // Write your code here
+      if(n1>n2)
+
+    {
+
+        int s=n1-n2;
+
+        for(int i=1;i<=10;i++)
+
+        {
+
+            cout<<s*i<<" ";
+
+        }
+
+    }
+}
