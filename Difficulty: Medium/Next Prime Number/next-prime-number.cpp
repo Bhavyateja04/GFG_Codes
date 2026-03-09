48d37847167f28d25faaa49b385fd0312ff9686e
Bhavya Teja Penke <147074331+Bhavyateja04@users.noreply.github.com>
2026-03-09 17:29:29 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Next Prime Number/next-prime-number.cpp
@@ -0,0 +1,26 @@
+// User function Template for C++
+
+int nextPrime(int n) {
+
+    // code here to find next prime number
+     jump: n=n+1;
+    bool flag=true;
+    while(true){
+
+        if(n<=1){
+            flag=false;
+        }
+        else{
+            for(int i{2};i<=sqrt(n);i++){
+                if(n%i==0){
+                    flag=false;
+                    goto jump;
+                 //  break;
+                }
+            }
+        }
+        if(flag==true){
+            return n;
+        }
+    }
+}
