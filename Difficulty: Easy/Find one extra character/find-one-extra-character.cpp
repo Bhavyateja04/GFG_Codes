3db3010bcd1d76accc497ff2e0f3980716516848
Bhavya Teja Penke <147074331+Bhavyateja04@users.noreply.github.com>
2026-03-20 09:10:36 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Find one extra character/find-one-extra-character.cpp
@@ -0,0 +1,16 @@
+// User function Template for C++
+
+char extraChar(string &s1, string &s2) {
+
+    // code here
+      char result =0;
+    
+    for(char c: s1){
+        result ^=c;
+    }
+    for(char c : s2){
+        result ^= c;
+        
+    }
+    return result ;
+}
