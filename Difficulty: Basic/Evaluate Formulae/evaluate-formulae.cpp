18971fa712533821326c6a1d67a605f35dd945c1
Bhavya Teja Penke <147074331+Bhavyateja04@users.noreply.github.com>
2026-03-29 22:13:47 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Evaluate Formulae/evaluate-formulae.cpp
@@ -0,0 +1,11 @@
+class Solution {
+  public:
+    int calculate(int a, int b, int c, int d) {
+        // Code here
+         int l = a+b;
+        int m = l/c;
+        int n = m+d;
+        
+        return n;
+    }
+};
