80644d074acfd01c3a4ccd09b37665451eb05f41
Bhavya Teja Penke <147074331+Bhavyateja04@users.noreply.github.com>
2026-04-01 22:05:58 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Greatest of Three/greatest-of-three.cpp
@@ -0,0 +1,22 @@
+// User function Template for C++
+
+int find_greatest_number(int a, int b, int c) {
+    // Write your code here..
+     if (a >= b) {
+        if (a >= c) {
+            return a;
+        }
+        else {
+            return c;
+        }
+    }
+    else {
+        if (b >= c) {
+            return b;
+        }
+        else {
+            return c;
+        }
+    }
+    return 0;
+}
