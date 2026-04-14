a0fe6d048c5fb4eacc5a0b778c77075d04832000
Bhavya Teja Penke <147074331+Bhavyateja04@users.noreply.github.com>
2026-04-14 22:27:42 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Divisor/divisor.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+    void printDivisors(int n) {
+        // code here
+          for(int i=1;i<=n;i++)
+        {
+            if(n%i==0)
+            {
+                cout<<i<<" ";
+            }
+        }
+    }
+};
