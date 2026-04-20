5b16f678eeb7dbfbb446b1500e800f1267e4836f
Bhavya Teja Penke <147074331+Bhavyateja04@users.noreply.github.com>
2026-04-20 19:51:00 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Global variable/global-variable.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+// declare a global variable g
+
+int main() {
+    // printing the value of global variable g
+    int g=10;
+    cout << g << endl;
+
+    return 0;
+}
