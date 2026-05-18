ec210b1b8419bd58e5fb0a054c4486917a31d215
Bhavya Teja Penke <147074331+Bhavyateja04@users.noreply.github.com>
2026-05-18 11:29:38 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Maximum Sum Problem/maximum-sum-problem.cpp
@@ -0,0 +1,18 @@
+class Solution {
+  public:
+    unordered_map<int, int> dp;
+    
+    int solve(int n) {
+        if(n == 0) return 0;
+        
+        if(dp.count(n)) return dp[n];
+        
+        int breakSum = solve(n/2) + solve(n/3) + solve(n/4);
+        
+        return dp[n] = max(n, breakSum);
+    }
+  
+    int maxSum(int n) {
+        return solve(n);
+    }
+};
