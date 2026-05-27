f4a3c36c4caf8c4c80b4de577e0bceabcd34fa88
Bhavya Teja Penke <147074331+Bhavyateja04@users.noreply.github.com>
2026-05-27 15:38:50 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Wifi Range/wifi-range.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool wifiRange(string &s, int x) {
+
+        int n = s.length();
+
+        vector<int> diff(n + 1, 0);
+
+        for (int i = 0; i < n; i++) {
+
+            if (s[i] == '1') {
+
+                int l = max(0, i - x);
+                int r = min(n - 1, i + x);
+
+                diff[l]++;
+                
+                if (r + 1 < n)
+                    diff[r + 1]--;
+            }
+        }
+
+        int coverage = 0;
+
+        for (int i = 0; i < n; i++) {
+
+            coverage += diff[i];
+
+            if (coverage <= 0)
+                return false;
+        }
+
+        return true;
+    }
+};
