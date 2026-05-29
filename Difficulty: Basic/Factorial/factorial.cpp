cfb0f996fc807f51868787f97a1086280606d8c7
Bhavya Teja Penke <147074331+Bhavyateja04@users.noreply.github.com>
2026-05-29 15:04:58 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Factorial/factorial.cpp
@@ -0,0 +1,11 @@
+class Solution {
+  public:
+    int factorial(int n) {
+        // code here
+        int res=1;
+        for(int i=1;i<=n;i++){
+            res=res*i;
+        }
+        return res;
+    }
+};
