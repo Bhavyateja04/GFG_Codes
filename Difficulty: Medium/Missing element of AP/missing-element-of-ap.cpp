2e90f9b0be1889c151dd2cad2662557f8e503939
Bhavya Teja Penke <147074331+Bhavyateja04@users.noreply.github.com>
2026-07-26 22:54:06 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Missing element of AP/missing-element-of-ap.cpp
@@ -0,0 +1,12 @@
+class Solution {
+  public:
+    int findMissing(vector<int> &arr) {
+        // code here
+        int n = arr.size();
+int diff = (arr[1]-arr[0]);
+for (int i = 2; i<n; i++) {
+if ((arr[i]-diff) != arr[i - 1]) {return arr[i]-diff; }
+}
+return arr[n - 1]+diff; 
+    }
+};
